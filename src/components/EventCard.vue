e3587b54e0eb8638fccab29bcc28f21b4709d2a0
Bohdan Leonets <leonetsbogdan@gmail.com>
2021-12-18 15:18:58 +0200

    vue-router and redirecting

---
+++ src/components/EventCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="true"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    propName: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      quote: 'Single quotes',
+    };
+  },
+};
+</script>
+
+<style></style>
