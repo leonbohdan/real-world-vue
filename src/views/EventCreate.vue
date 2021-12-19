f1c23935d778be76290a1ad0e3ea5d4c38d1be46
Bohdan Leonets <leonetsbogdan@gmail.com>
2021-12-19 18:10:40 +0200

    import { mapState } from "vuex"

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,11 +1,35 @@
 <template>
   <div>
-    <h1>Create Event</h1>
+    <h1>Create Event, {{ user.name }}</h1>
+
+    <p>This Event was created by {{ user.id }}</p>
+
+    <ul>
+      <li v-for="(category, index) in categories" :key="index">
+        {{ category }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {};
+import { mapState } from 'vuex';
+
+export default {
+  // computed: {
+  //   userName() {
+  //     return this.$store.state.user.name;
+  //   },
+  //   userId() {
+  //     return this.$store.state.user.id;
+  //   },
+  // },
+  // computed: mapState({
+  //   user: 'user',
+  //   categories: 'categories',
+  // }),
+  computed: mapState(['user', 'categories']),
+};
 </script>
 
-<style lang="sass"></style>
+<style></style>
